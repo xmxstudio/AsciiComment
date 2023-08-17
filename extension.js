f665a72e51f793ea3c3a2b18cbe815bebc9e25c4
xmxstudio <xmetrix@gmail.com>
2023-08-17 06:47:55 -0700

    Config section added

--- extension.js
+++ extension.js
@@ -1,8 +1,43 @@
 const vscode = require("vscode");
 const { execSync } = require("child_process");
 
+// function activate(context) {
+//   console.log("AsciiComment Extension is now active.");
+
+//   let disposable = vscode.commands.registerCommand(
+//     "extension.asciiArtConvert",
+//     async () => {
+//       const editor = vscode.window.activeTextEditor;
+
+//       if (!editor) {
+//         return;
+//       }
+
+//       const currentLine = editor.document.lineAt(editor.selection.active.line);
+//       const selectedText = currentLine.text;
+
+//       try {
+//         const asciiArt = execSync(`echo "${selectedText}" | toilet -F border -f future`, {encoding: "utf-8"});
+
+//         const commentAsciiArt = `${asciiArt.trim().split("\n").map(x=>`// ${x}`).join("\n")}`;
+
+//         await editor.edit((editBuilder) => {
+//           const selectionRange = new vscode.Range(
+//             currentLine.range.start,
+//             currentLine.range.end
+//           );
+//           editBuilder.replace(selectionRange, commentAsciiArt);
+//         });
+//       } catch (error) {
+//         vscode.window.showErrorMessage("Error converting to AsciiComment.");
+//       }
+//     }
+//   );
+
+//   context.subscriptions.push(disposable);
+// }
 function activate(context) {
-  console.log("AsciiComment Extension is now active.");
+  // ...
 
   let disposable = vscode.commands.registerCommand(
     "extension.asciiArtConvert",
@@ -16,20 +51,32 @@ function activate(context) {
       const currentLine = editor.document.lineAt(editor.selection.active.line);
       const selectedText = currentLine.text;
 
-      try {
-        const asciiArt = execSync(`echo "${selectedText}" | toilet -F border -f future`, {encoding: "utf-8"});
+      if (!selectedText) {
+        return;
+      }
 
+      const config = vscode.workspace.getConfiguration("asciicomment");
+      console.log(config);
+
+      const asciiArtCommand = config.get("asciiArtCommand", "");
+console.log(asciiArtCommand);
+      try {
+        const asciiArt = execSync(asciiArtCommand.replace("${currentLine}", selectedText), {
+          encoding: "utf-8",
+        });
         const commentAsciiArt = `${asciiArt.trim().split("\n").map(x=>`// ${x}`).join("\n")}`;
 
-        await editor.edit((editBuilder) => {
-          const selectionRange = new vscode.Range(
-            currentLine.range.start,
-            currentLine.range.end
-          );
-          editBuilder.replace(selectionRange, commentAsciiArt);
-        });
+                await editor.edit((editBuilder) => {
+                  const selectionRange = new vscode.Range(
+                    currentLine.range.start,
+                    currentLine.range.end
+                  );
+                  editBuilder.replace(selectionRange, commentAsciiArt);
+                });
       } catch (error) {
-        vscode.window.showErrorMessage("Error converting to AsciiComment.");
+        console.log(error);
+        // vscode.window.showErrorMessage("Error converting to AsciiComment.");
+        vscode.window.showErrorMessage(error);
       }
     }
   );
